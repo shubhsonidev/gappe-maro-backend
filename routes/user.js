bf23fd3466bec372f6a0ec196b3db9886baf9105
Shubh soni <shubhsoni.dev@gmail.com>
2024-09-18 18:11:12 +0530

    changing method

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,12 @@ const express = require("express");
 const { hangleGenerateOTP, handleVerifyOTP, handleUserUpdate, handleUserSearch } = require("../controllers/user");
 const router = express.Router();
 
-router.get("/generateOtp", hangleGenerateOTP);
+router.post("/generateOtp", hangleGenerateOTP);
 
 router.post("/verify", handleVerifyOTP);
 
 router.put("/update", handleUserUpdate);
 
-router.get("/search", handleUserSearch);
+router.post("/search", handleUserSearch);
 
 module.exports = router;
